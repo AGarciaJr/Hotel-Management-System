99de45de50451e090c1dfce9a4f9cc0fd7b550c6
Alex <173425531+AGarciaJr@users.noreply.github.com>
2024-09-18 11:47:20 -0500

    User methods!

--- src/main/java/users/user.java
+++ src/main/java/users/user.java
@@ -1,5 +1,26 @@
 package users;
 
 public class user {
-    public String name;
+    private String email;
+    private String password;
+
+    public user(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
